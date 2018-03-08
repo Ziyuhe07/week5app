bdd5d714cdbff769210ca8b7c3fa623951878b2a
Ziyuhe07 <ucfazhe@ucl.ac.uk>
2018-03-08 09:34:54 -0500

    CallDiv function

--- ucfazhe/www/js/appActivity.js
+++ ucfazhe/www/js/appActivity.js
@@ -87,28 +87,4 @@ alert("Earthquake data will be removed");
 mymap.removeLayer( earthquakelayer );
 }
 
-var xhr; //define the global variables to process the AJAX request
-function callDivChange() {
-	xhr = new XMLHttpRequest();
-	var filename = document.getElementById("filename").value;
-	xhr.open("GET", filename, true);
-	xhr.onreadystatechange = processDivChange;
-	try {
-		xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-	}
-	catch (e) {
-			// this only works in internet explorer
-	}
-	xhr.send();
-}
 
-function processDivChange(){
-	if (xhr.readyState <4)   //while waiting response from server
-	document.getElementById('div1').innerHTML = "loading...";
-	
-	else if (xhr.readyState == 4){    //4 = Response from server has been completely loaded.
-		if (xhr.status == 200 && xhr.status < 300)
-			//http status between 200 to 299 are all successful
-		document.getElementById('div1').innerHTML = xhr.responseText;
-	}
-}
